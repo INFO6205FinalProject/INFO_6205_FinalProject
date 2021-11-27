a57473dcbb966717179bb0625cce61b3ffc2381c
Jim-Qin <1049102910@qq.com>
2021-11-27 14:29:49 -0500

    read & write txt

--- sort/src/INFO_6205_FinalProject/TXT.java
+++ sort/src/INFO_6205_FinalProject/TXT.java
@@ -25,12 +25,14 @@ public class TXT {
             e.printStackTrace();
         }
     }
-    public static void write_txt(String s){
+    public static void write_txt(String add, String s[]){
         try {
-            File writename = new File(s); // 相对路径，如果没有则要建立一个新的output。txt文件
+            File writename = new File(add); // 相对路径，如果没有则要建立一个新的output。txt文件
             writename.createNewFile(); // 创建新文件
             BufferedWriter out = new BufferedWriter(new FileWriter(writename));
-            out.write("我会写入文件啦\r\n"); // \r\n即为换行
+            for(String i : s){
+                out.write(i + "\r\n");
+            }
             out.flush(); // 把缓存区内容压入文件
             out.close(); // 最后记得关闭文件
         } catch (Exception e) {
