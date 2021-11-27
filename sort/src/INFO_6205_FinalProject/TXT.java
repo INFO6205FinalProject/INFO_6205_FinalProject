7b1009228f155f0ef08662386c0faac33a927acb
Jim-Qin <1049102910@qq.com>
2021-11-27 15:26:59 -0500

    read & write txt

--- sort/src/INFO_6205_FinalProject/TXT.java
+++ sort/src/INFO_6205_FinalProject/TXT.java
@@ -5,13 +5,15 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TXT {
-    public static void read_txt(String s){
+    public static ArrayList<String> read_txt(String add){
+        ArrayList<String> s = new ArrayList<>();
         try { // 防止文件建立或读取失败，用catch捕捉错误并打印，也可以throw
-
             /* 读入TXT文件 */
-            String pathname = s; // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径
+            String pathname = add; // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径
             File filename = new File(pathname); // 要读取以上路径的input。txt文件
             InputStreamReader reader = new InputStreamReader(
                     new FileInputStream(filename)); // 建立一个输入流对象reader
@@ -20,17 +22,20 @@ public class TXT {
             line = br.readLine();
             while (line != null) {
                 line = br.readLine(); // 一次读入一行数据
+                s.add(line);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return s;
     }
-    public static void write_txt(String add, String s[]){
+    public static void write_txt(String add, ArrayList s){
         try {
             File writename = new File(add); // 相对路径，如果没有则要建立一个新的output。txt文件
             writename.createNewFile(); // 创建新文件
             BufferedWriter out = new BufferedWriter(new FileWriter(writename));
-            for(String i : s){
+            for(Object i : s){
+                i = (String)(i);
                 out.write(i + "\r\n");
             }
             out.flush(); // 把缓存区内容压入文件
